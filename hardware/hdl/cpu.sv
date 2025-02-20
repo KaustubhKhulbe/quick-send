53a95226c80c960717335657e5ce7b035f2b1d31
kkhulbe2 <kkhulbe2@linux-ssh-06.ews.illinois.edu>
2025-02-20 17:30:27 -0600

    synth works?

--- hardware/hdl/cpu.sv
+++ hardware/hdl/cpu.sv
@@ -3,15 +3,16 @@ module cpu
     input   logic           clk,
     input   logic           rst,
 
-    input   logic   [31:0]  lookup_pc
+    output  logic           tmp
 );
 
     bit tmp;
     always_ff @(posedge clk) begin
         if (rst) begin
-           if (lookup_pc == 32'h00000000) begin
-                tmp <= 1'b1;
-           end
+            tmp <= 1'b1;
+        end
+        else begin
+            tmp <= 1'b0;
         end
     end
 
