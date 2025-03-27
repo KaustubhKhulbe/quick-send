ba90abdb63c4beef20d1cb8238a552bdebd4fd1d
urd00m <alan.linghao.wang@gmail.com>
2025-03-26 23:38:54 -0500

    validating against image from trisha's dataset and fixing some bugs in the code

--- test_script.py
+++ test_script.py
@@ -7,6 +7,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import pdb
+import tifffile
 
 if __name__ == '__main__':
 
@@ -25,11 +26,17 @@ if __name__ == '__main__':
     a = np.tile((np.cos(np.linspace(0, np.pi, height)) * 255).astype(np.uint8).reshape(height, 1), (1, width))  # Cosine Alpha fading
 
     gradient_image = np.stack([r, g, b, a], axis=0)  # (4, 512, 512)
+    image = Image.fromarray(np.moveaxis(gradient_image, 0, -1), mode="RGBA")
+    image.save("gradient_alpha_fixed.png")
 
-    # image = Image.fromarray(np.moveaxis(gradient_image, 0, -1), mode="RGBA")
-    # image.save("harness/gradient_alpha_fixed.png")
+    # Image is missing the A channel
+    image = tifffile.imread('dataset/4.2.07.tiff')
+    image = np.transpose(np.array(image, dtype=np.uint8))
+    image = np.concatenate((image, np.full((1, image.shape[1], image.shape[2]), 255, dtype=np.uint8)), axis=0)
+    temp = Image.fromarray(np.moveaxis(image, 0, -1), mode="RGBA")
+    temp.save("test.png")
 
-    dataset = np.array([gradient_image])
+    dataset = np.array([gradient_image, image])
     # TODO none needs to be replaced with something
     mali_gpu = aut.Aut(dataset, mali_compression.mali_compression, mali_compression.mali_decompression, None, name="Mali GPU") 
     mali_gpu.formal_validate()
